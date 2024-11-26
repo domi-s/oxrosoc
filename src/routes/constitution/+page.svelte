460b9c7ce3a68b0508dc0996d8df9386f8d78250
Dominic Satnoianu <44111026+domi-s@users.noreply.github.com>
2024-11-26 16:29:47 +0000

    Changed consti

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -2,7 +2,7 @@
     import Document from "$lib/components/Document.svelte";
 
     let content = `
-        <p>You can view and download the Constitution of the Oxford Romanian Society at the following link: <a href='/documents/constitution.docx' target='_blank'>OxRoSoc Constitution</a>.</p>
+        <p>You can view and download the Constitution of the Oxford Romanian Society at the following link: <a href='/documents/constitution-26-11-2024.docx' target='_blank'>OxRoSoc Constitution</a>.</p>
     `;
 </script>
 
