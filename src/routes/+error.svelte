2aa3496d4e890dcfa98e7672ccec26232c369b1b
domi-s <44111026+domi-s@users.noreply.github.com>
2026-04-22 17:49:52 +0100

    Update +error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,6 +2,6 @@
     <div class="py-16 flex flex-col items-center justify-center text-center gap-4">
         <h1 class="text-4xl font-bold">404 &ndash; Page Not Found</h1>
         <p class="text-lg text-gray-500">Sorry, we couldn't find the page you're looking for.</p>
-        <a href="/" class="w-fit bg-red-200 border-red-300 border-2 border-solid text-red-700 px-3 py-1 rounded-md hover:bg-red-300">Back to Homepage</a>
+        <a href="/" class="w-fit bg-red-200 border-red-300 border-2 border-solid text-red-700 px-4 py-2 rounded-md hover:bg-red-300">&lsaquo;&nbsp; Back to Homepage</a>
     </div>
 </div>
